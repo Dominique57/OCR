2b0be678dbc57b1932d0cd60970052a61bb7c8c9
simon <Simon Guiot>
2018-10-13 15:08:55 +0200

    added print_Array() in main

--- main.c
+++ main.c
@@ -1,10 +1,28 @@
 #include "stdio.h"
 #include "Bmp_Parser.h"
 
+
+
+void print_Array(unsigned char array[], size_t w, size_t h) //prints all values from an array
+{
+	size_t k;
+	k=0;
+	for(size_t i=0; i<w; i++)
+	{
+			for (size_t j=0; j<h; j++)
+			{
+				printf("%d|",array[k]);
+				k++;
+			}
+			printf("\n");
+	}
+}
+
+
 int main ()
 {
 
-	char path[]="~/Test.bmp";
+	char path[]="TEST3.bmp";
 
 
 	//not very optimized, feel free to modify if you have an idea
@@ -16,8 +34,9 @@ int main ()
 
 	if (parse_bmp(final_array, path)==1)
 	{
-		printf("File not found !");
+		printf("File not found !\n");
 		return 1;
 	}
+	//print_Array(final_array, width, height); //debug
 	return 0;
 }
